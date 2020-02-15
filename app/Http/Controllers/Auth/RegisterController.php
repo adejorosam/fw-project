25aa67905fdaf9624072e5a0dd586886211b2dab
Samson <samsonadejoro@gmail.com>
2020-02-15 01:09:21 +0100

    Welcome Message on Sign Up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeNewUserMail;
+use App\Events\NewCustomerHasRegisteredEvent;
+
 
 class RegisterController extends Controller
 {
@@ -67,9 +71,10 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            
 
         ]);
+        event(new NewCustomerHasRegisteredEvent($user));
+        // Mail::to($user->email)->send(new WelcomeNewUserMail());
         $user->attachRole('Student');
         return $user;
     }
---
+++ app/Listeners/WelcomeNewUser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeNewUserMail;
+
+
+class WelcomeNewUser
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+        Mail::to($event->user->email)->send(new WelcomeNewUserMail());
+    }
+}
