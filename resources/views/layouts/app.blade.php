4047e20d2bc312598c688c40a155c3e87e7705d4
Samson <samsonadejoro@gmail.com>
2020-03-29 20:02:27 +0100

    Bootstrap addition

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
   
     
 </head>
