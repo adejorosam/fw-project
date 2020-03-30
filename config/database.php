17c9b765c9d92a6a7683e288bca331b2efefff45
Samson <samsonadejoro@gmail.com>
2020-03-30 03:19:28 +0100

    DB Changes!

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            // 'url' => env('DATABASE_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => $host,
             'port' => env('DB_PORT', '3306'),
             'database' => $database,
