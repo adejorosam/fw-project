e7c04eb4fc7f99ea04aefae41043a76ccceee17b
Samson <samsonadejoro@gmail.com>
2020-04-30 08:37:03 +0100

    Reset Password refactor

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/userdashboard';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
